7d6ce74ec5cfce6d09cf61690ecc8193365371cc
AdMyNyRaL AdMyNyRaL <jion93@mail.ru>
2019-10-06 12:26:31 +0300

    Deleted html

--- src/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"bean","l":"Digit"},{"p":"run","l":"Program"},{"p":"bean","l":"SortDigits"},{"p":"action","l":"TaskExecutor"}]
